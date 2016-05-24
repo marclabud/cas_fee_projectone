ef4660bf40961390c1e5c87a2b3985ac20830ef3
Marc Labud <marc.labud@hsr.ch>
2016-05-24 19:35:56 +0200

    handlebar working layout issue

--- noteslistview.ts
+++ noteslistview.ts
@@ -1,10 +1,22 @@
 "use strict";
 
-var notes = [
-    {"id":"11", "title":"Erste Notiz", "description": "Erste Beschreibung"},
-    {"id":"12", "title":"Zweite Notiz", "description": "Zweite Beschreibung"}
-];
-var createNotesHTML = Handlebars.compile(document.getElementById("notes-template").innerText);
-/*function renderNotes () {
-    $.("#notes").html(createNotesHTML(notes));
-}*/
+window.onload = function() {
+    console.log ("Start Window onload");
+
+    var notesarray = [
+        {id:"11", title:"Erste Notiz", description: "Erste Beschreibung"},
+        {id:"12", title:"Zweite Notiz", description: "Zweite Beschreibung"},
+        {id:"13", title:"Dritte Notiz", description: "Dritte Beschreibung"},
+        {id:"14", title:"Vierte Notiz", description: "Vierte Beschreibung"}
+    ];
+    var context = {
+        notes : notesarray
+    };
+    var createNotesHTML: HandlebarsTemplateDelegate = Handlebars.compile(document.getElementById("notes-template").innerText);
+    
+    var listHtml : string  = createNotesHTML(context);
+    document.getElementById("notes-list").innerHTML = listHtml;
+
+    console.log ("End Window onload");
+};
+
