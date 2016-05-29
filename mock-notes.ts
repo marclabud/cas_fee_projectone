db091e1f700dd0f005e03e6cc753c1ef51b2fa69
Marc Labud <marc.labud@hsr.ch>
2016-05-29 19:58:38 +0200

    note-card refactoring css styles without use deleted mock data reformatted

--- mock-notes.ts
+++ mock-notes.ts
@@ -3,26 +3,51 @@
  */
 
 interface note {
-    id: number;
-    title: string;
-    description: string;
-    importance: number;
-    finishedDate: String;
-    createdDate: String;
-    dueDate: String;
+    id:number;
+    title:string;
+    description:string;
+    importance:number;
+    finishedDate:String;
+    createdDate:String;
+    dueDate:String;
 }
 
-var notesarray: note[]= [
-    {"id":11, title:"Erste Notiz", description: "Erste Beschreibung", importance: 1, finishedDate:"05/11/2016", createdDate: "05/01/2016", dueDate: "05/21/2016"},
-    {"id":12, title:"Zweite Notiz", description: "Zweite Beschreibung", importance: 2, finishedDate:"05/12/2016", createdDate: "05/02/2016", dueDate: "05/22/2016"},
+var notesarray:note[] = [
+    {
+        "id": 11,
+        title: "Erste Notiz",
+        description: "Erste Beschreibung",
+        importance: 1,
+        finishedDate: "05/11/2016",
+        createdDate: "05/01/2016",
+        dueDate: "05/21/2016"
+    },
+    {
+        "id": 12,
+        title: "Zweite Notiz",
+        description: "Zweite Beschreibung",
+        importance: 2,
+        finishedDate: "05/12/2016",
+        createdDate: "05/02/2016",
+        dueDate: "05/22/2016"
+    },
     {
         "id": 13,
         title: "Dritte Notiz",
         description: "Dritte Beschreibung",
-        importance: 0,
+        importance: 4,
         finishedDate: "05/13/2016",
         createdDate: "05/03/2016",
         dueDate: "05/23/2016"
     },
-    {"id":14, title:"Vierte Notiz", description: "Vierte Beschreibung", importance: 4, finishedDate:"05/14/2016", createdDate: "05/04/2016", dueDate: "05/24/2016"},
+
+    {
+        "id": 15,
+        title: "Fünfte Notiz",
+        description: "Fünfte lange Beschreibung",
+        importance: 5,
+        finishedDate: "",
+        createdDate: "05/04/2016",
+        dueDate: "05/24/2016"
+    },
 ];  
