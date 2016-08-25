93ecfb63a5bd2973b7904ab1be8d2778b9d88b89
Marc Labud <marc.labud@hsr.ch>
2016-08-25 17:14:33 +0200

    gulp task hello, tsc, sass, build package.json script build

---
+++ gulp/task/build.js
@@ -0,0 +1,11 @@
+'use strict';
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+
+gulp.task('build',['ts', 'sass'], build);
+
+function build() {
+    gutil.log('Build Tasks Done')
+}
+build.description=`Build Stand Release 1.0 tsc und sass`;
+build.flags= {'--dev':`Coming soon`};
