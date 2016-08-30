c46d3797046ad007cedf7ffa1284651e7d9ea457
Marc Labud <marc.labud@hsr.ch>
2016-08-30 08:32:50 +0200

    New task lib for tcal

---
+++ gulp/task/lib.js
@@ -0,0 +1,14 @@
+'use strict';
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+
+gulp.task('lib', lib);
+
+function lib() {
+    gulp.src('./src/lib/*')
+        .on('error', gutil.log)
+        .pipe(gulp.dest('./public/lib'));
+
+}
+lib .description=`all libs: copy from src to public`;
+
